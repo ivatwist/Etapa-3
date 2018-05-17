bfad44711a701b55bdc1326ab9946dfa47449686
ivatwist <39283123+ivatwist@users.noreply.github.com>
2018-05-17 10:02:55 -0600

    holiwis cambielo
    holiwis

--- Algoritmos.h
+++ Algoritmos.h
@@ -15,5 +15,25 @@ Algortimos para lista posicionada, indexada y ordenada*/
 class Algoritmos{
   Algoritmos();
   
+  public:
+  void listarIndexada(ListaI); //completada
+bool simetricaIndexada(ListaI); //completada
+bool buscarIndexada(int, ListaI); //completada
+void invertirIndexada(ListaI&); //completada
+void eliminarRepetidosIndexada(ListaI&); //completada
+bool subListaIndexada(ListaI, ListaI); //completada
+bool igualesIndexada(ListaI, ListaI); //completada
+void burbujaOriginalIndexada(ListaI&); //completada
+void burbujaBidireccionalIndexada(ListaI&);
+void seleccionIterativaIndexada(ListaI&); //completada
+void seleccionRecursivaIndexada(ListaI&, int);
+void seleccionRecursivaPilaIndexada(ListaI&);
+void insercionIndexada(ListaI);
+void quickSortAhoIndexada(ListaI);
+void quickSortIndexada(ListaI); //si la sublista tiene menos de 50 elementos se usa insercion
+void mergeSortIndexada(ListaI);
+void unionListasIndexadas(ListaI);
+void unionListasDesordenadasIndexadas(ListaI);
+  
 };
 #endif
